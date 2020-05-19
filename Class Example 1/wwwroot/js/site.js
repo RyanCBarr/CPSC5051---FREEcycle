c4fb5ebe2d24c8c7a73a2785d98cb94dea7aea31
DESKTOP-1L84BIH\Ryan <RyanC.Barr@gmail.com>
2020-05-19 07:51:58 -0700

    Added basic floating menu for KF1 (css+js). TODO: Ensure it's on TOP of all other elements. TODO: Make pretty.

--- Class Example 1/wwwroot/js/site.js
+++ Class Example 1/wwwroot/js/site.js
@@ -2,3 +2,12 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+$(window).scroll(function () {
+    if ($(window).scrollTop() >= 200) {
+        $("#floatbar").css({ position: 'fixed', left: '0', top: '0' });
+    }
+    else {
+        $("#floatbar").css({ position: 'absolute', left: '0', top: '74px' });
+    }
+});
