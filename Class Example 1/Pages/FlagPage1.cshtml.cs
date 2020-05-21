5de45c25efa4ee036f7222c6825814267b997e30
markplotlib <markplotlib@gmail.com>
2020-05-21 14:21:05 -0700

    add flag page 1

---
+++ Class Example 1/Pages/FlagPage1.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Class_Example_1.Pages
+{
+    public class FlagPage1Model : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
